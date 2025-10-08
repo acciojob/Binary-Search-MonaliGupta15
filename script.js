c7b198bb845be0ca009929fc31b0028dd466e8cf
Monali Gupta <monaligupta06@gmail.com>
2025-10-08 18:00:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ function binarySearch(nums, target) {
 // Your code here
 // Return index if target is present
 // else return -1
+	let s=0
+	let e = nums.length-1
+	let ispresent =false
+	while(s <= e)
+		{
+			let mid = Math.floor((s+e)/2)
+
+			if(arr[mid] == target)
+			{
+				ispresent = true
+				console.log(mid)
+				break
+			}
+			else if(target > arr[mid])
+			{
+				s = mid + 1
+			}
+			else
+			{
+				end = mid -1
+			}
+		}
+}
+if(ispresent == false)
+{
+	console.log(-1)
 }
 
 module.exports = binarySearch;
